e3ae197216d39327dd61a14f17ddcfafacb6c844
GauravGaur-957 <62003047+GauravGaur-957@users.noreply.github.com>
2020-05-20 10:10:32 +0530

    Update sms_python.py

--- Projects/sms_python.py
+++ Projects/sms_python.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 
-account = "AC08505b462458dce150355a81bd4f572f"
-token = "2cc1b398cc099ef2cf30f9915c408945"
+account = "#account"
+token = "#token"
 client = Client(account, token)
 
 message = client.messages.create(
